5e93634f71f7698367eee1a71acfabc7a206c26f
Kelly Amaya <kjamayaca@cesde.net>
2023-05-24 11:22:53 -0500

    Se crea proyecto universidad para crear componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
+//Aquí muestra un resumen de todo el proyecto 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
